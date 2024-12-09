c19bc2909876b7c87d848eb743737ba4c6ebce19
Nay Linn <naylinn@nays-mbp.mynetworksettings.com>
2024-12-09 16:46:50 -0500

    admin cache removed

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@ import FormsCustomization from "@/components/FormsCustomization/FormsCustomizati
 import { ComponentSteps } from "@/types";
 export default async function Page() {
   const url = `${process.env.NEXT_PUBLIC_BASE_URL}/components`;
-  const data = await fetch(url);
+  const data = await fetch(url, { cache: "no-store" });
   const components: ComponentSteps[] = await data.json();
 
   return <FormsCustomization components={components} />;
