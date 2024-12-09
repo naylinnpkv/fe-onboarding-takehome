99735f5f027e33736f6d88693f4b4e55e1f8727c
Nay Linn <naylinn@nays-mbp.mynetworksettings.com>
2024-12-09 16:36:21 -0500

    no cache added to ssr

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -2,7 +2,8 @@ import UserTable from "@/components/UserTable";
 import { User } from "@/types";
 
 export default async function Page() {
-  const data = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/users`);
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/users`;
+  const data = await fetch(url, { cache: "no-store" });
   const users: User[] = await data.json();
   return <UserTable users={users} />;
 }
